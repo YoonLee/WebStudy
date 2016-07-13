b90cd8a4fd1f604eed2d7a5ca7279175c35c22ae
Yoon Lee <yoon.lee.uci@gmail.com>
2016-07-13 14:38:30 -0700

    schema and right bcript module added

--- query.js
+++ query.js
@@ -1,15 +1,20 @@
 function createAccount (connection, fname, lname, email, password, phone, callback) {
-	var bcrypt = require('bcrypt');
+	var bcrypt = require('bcryptjs');
 	var salt	= bcrypt.genSaltSync(10);
 	var hash	= bcrypt.hashSync(password, salt);
 	var createStmt = 'INSERT INTO xuser (email, password, fname, lname, phone) VALUES (?, ?, ?, ?, ?)';
 	connection.query(createStmt, [email, hash, fname, lname, phone], function (err, rows, fields) {
-
+		if (!err) {
+			console.log('created');
+		}
+		else {
+			console.log('not created');
+		}
 	});
 }
 
 function webAuth (connection, email, password, callback) {
-	var bcrypt = require('bcrypt');
+	var bcrypt = require('bcryptjs');
 	var authStmt= 'SELECT password FROM xuser WHERE email=?';
 	connection.query(authStmt, [email, password], function (err, rows, fields) {
 		if (!err) {
